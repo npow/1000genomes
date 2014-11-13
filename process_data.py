b8f3627d152d6e628e352f357fb5f09cdd2a2727
npow <nissan.pow@gmail.com>
2014-11-12 22:18:23 -0500

    .

--- process_data.py
+++ process_data.py
@@ -1,9 +1,37 @@
 import numpy as np
+import tables as tb
 from scipy import sparse
 from sklearn.externals import joblib
 import h5py
 import gzip
 import sys
+
+def store_sparse_matrix(m, name):
+    msg = "This code only works for csr matrices"
+    assert(m.__class__ == sparse.csr.csr_matrix), msg
+    store = 'blobs/%s.h5' % name
+    with tb.openFile(store,'a') as f:
+        for par in ('data', 'indices', 'indptr', 'shape'):
+            full_name = '%s_%s' % (name, par)
+            try:
+                n = getattr(f.root, full_name)
+                n._f_remove()
+            except AttributeError:
+                pass
+
+            arr = np.array(getattr(m, par))
+            atom = tb.Atom.from_dtype(arr.dtype)
+            ds = f.createCArray(f.root, full_name, atom, arr.shape)
+            ds[:] = arr
+
+def load_sparse_matrix(name):
+    store = 'blobs/%s.h5' % name
+    with tb.openFile(store) as f:
+        pars = []
+        for par in ('data', 'indices', 'indptr', 'shape'):
+            pars.append(getattr(f.root, '%s_%s' % (name, par)).read())
+    m = sparse.csr_matrix(tuple(pars[:3]), shape=pars[3])
+    return m
 
 def get_num_features(file_name):
     f = gzip.open(file_name)
@@ -52,11 +80,8 @@ if DUMP_VCF:
                 X[idx, j] = v
         idx += 1
     print X.shape
-    #joblib.dump(X.T.tocoo(), 'blobs/X_%s.pkl' % CHR_NUM)
-    h5f = h5py.File('blobs/X_%s.h5' % CHR_NUM, 'w')
-    h5f.create_dataset('X_%s' % CHR_NUM, data=X.T.tocoo(), compression="gzip", compression_opts=9)
-    h5f.close()
     f.close()
+    store_sparse_matrix(X.T.tocoo(), 'X_%s' % CHR_NUM)
     sys.exit(0)
 
 if DUMP_LABELS:
