d4db7abb02eb44837d14d7fc1c48052e9f3ad1bf
npow <nissan.pow@gmail.com>
2014-11-14 10:21:02 -0500

    RF

--- classify.py
+++ classify.py
@@ -2,31 +2,43 @@ import numpy as np
 import scipy
 import sys
 from nolearn.dbn import DBN
-from sklearn.cross_validation import train_test_split
-from sklearn.decomposition import TruncatedSVD, PCA, RandomizedPCA
+from sklearn.cross_validation import *
+from sklearn.decomposition import *
+from sklearn.ensemble import *
 from sklearn.externals import joblib
 from sklearn.feature_selection import *
 from sklearn.linear_model import *
 from sklearn.neighbors import *
 from sklearn.svm import *
+from sklearn.tree import *
 from sklearn import metrics
 from utils import *
 
-X_21 = load_sparse_matrix('blobs/X_21.pkl')
-X_22 = load_sparse_matrix('blobs/X_22.pkl')
-X = scipy.sparse.hstack((X_21, X_22))
-del X_21
-del X_22
+def load(L):
+  return scipy.sparse.hstack([load_sparse_matrix(x) for x in L])
+
+chromosomes = [21, 22]
+X = load(["X_%d" % c for c in chromosomes])
 Y = joblib.load('blobs/Y_pop.pkl')
+print chromosomes
 print X.shape
 print Y.shape
+sys.stdout.flush()
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
 
-clf = LinearSVC()
-clf.fit(X_train, Y_train)
-print "DONE FIT"
-pred = clf.predict(X_test)
+#clf = LinearSVC(penalty='l2')
+clf = RandomForestClassifier(n_estimators=100, n_jobs=4)
+if clf in [RandomForestClassifier]:
+  clf.fit(X_train.todense(), Y_train)
+  print "DONE FIT"
+  sys.stdout.flush()
+  pred = clf.predict(X_test.todense())
+else:
+  clf.fit(X_train, Y_train)
+  print "DONE FIT"
+  sys.stdout.flush()
+  pred = clf.predict(X_test)
 
 score = metrics.f1_score(Y_test, pred)
 print("f1-score:   %0.3f" % score)
