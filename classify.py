3b20a7212e21811210e86f787d0ca32fde962f94
npow <nissan.pow@gmail.com>
2014-11-22 13:34:44 -0500

    use pipeline

--- classify.py
+++ classify.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
+from __future__ import division
 import numpy as np
 import scipy
 import sys
+from sklearn.base import BaseEstimator, TransformerMixin
 from nolearn.dbn import DBN
 from sklearn.cross_validation import *
 from sklearn.decomposition import *
@@ -9,6 +13,7 @@ from sklearn.externals import joblib
 from sklearn.feature_selection import *
 from sklearn.linear_model import *
 from sklearn.neighbors import *
+from sklearn.pipeline import *
 from sklearn.preprocessing import *
 from sklearn.svm import *
 from sklearn.tree import *
@@ -16,64 +21,73 @@ from sklearn import metrics
 from utils import *
 np.random.seed(42)
 
-PREFIX = "sgdclassifier_chr%s" % sys.argv[1]
-print PREFIX
+USE_STACKING = True
+
+if len(sys.argv) == 1:
+  print "No chromosomes specified"
+  sys.exit(1)
+
+class Transformer(BaseEstimator, TransformerMixin):
+    def __init__(self, fn):
+        self.fn = fn
+
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X):
+        return self.fn(X)
 
 def load(L):
   return scipy.sparse.hstack([load_sparse_matrix(x) for x in L])
 
-chromosomes = [int(x) for x in sys.argv[1].split(',')]
-print chromosomes
-sys.stdout.flush()
-if len(chromosomes) == 0:
-  print "No chromosomes specified"
-  sys.exit(1)
+def main():
+    chromosomes = [int(x) for x in sys.argv[1].split(',')]
+    print "Chromosomes: ", chromosomes
+    sys.stdout.flush()
 
-X = load(["X_%d" % c for c in chromosomes])
-Y = joblib.load('blobs/Y_pop.pkl')
-print X.shape
-print Y.shape
-sys.stdout.flush()
+    X = load(["X_%d" % c for c in chromosomes])
+    Y = joblib.load('blobs/Y_pop.pkl')
+    print X.shape
+    print Y.shape
+    sys.stdout.flush()
 
-le = LabelEncoder()
-Y = le.fit_transform(Y)
-joblib.dump(le, 'blobs/le.pkl')
+    le = LabelEncoder()
+    Y = le.fit_transform(Y)
+    joblib.dump(le, 'blobs/le.pkl')
 
-X_base, X_valid, Y_base, Y_valid = train_test_split(X, Y, test_size=0.1, random_state=42)
+    if USE_STACKING:
+        skf = StratifiedKFold(Y, n_folds=4, shuffle=True, random_state=42)
+        for train_indices, test_indices in skf:
+            X_train, Y_train = X[train_indices], Y[train_indices]
+            X_test, Y_test = X[test_indices], Y[test_indices]
 
-X_train_meta = []
-Y_train_meta = []
+            clf = Pipeline([
+                ('t', Transformer(LinearSVC().fit(X_train, Y_train).decision_function)),
+                ('c', LinearSVC()),
+            ])
 
-skf = StratifiedKFold(Y_base, n_folds=4, shuffle=True, random_state=42)
-for train_indices, test_indices in skf:
-  X_train, Y_train = X_base[train_indices], Y_base[train_indices]
-  X_test, Y_test = X_base[test_indices], Y_base[test_indices]
+            clf.fit(X_train, Y_train)
+            print "DONE FIT"
+            sys.stdout.flush()
 
-  clf = SGDClassifier(n_jobs=-1, n_iter=1)
-  clf.fit(X_train, Y_train)
-  print "DONE FIT"
-  sys.stdout.flush()
-  X_train_meta.append(clf.decision_function(X_test))
-  Y_train_meta.append(Y_test)
+            pred = clf.predict(X_test)
 
-clf = SGDClassifier(n_jobs=-1, n_iter=1)
-clf.fit(X_base, Y_base)
-X_test_meta = clf.decision_function(X_valid)
-X_train_meta = np.concatenate(X_train_meta, axis=0)
-Y_train_meta = np.concatenate(Y_train_meta, axis=0)
-print "DONE CREATING META"
+            score = metrics.f1_score(Y_test, pred)
+            print("f1-score:   %0.3f" % score)
 
-clf = SGDClassifier(n_jobs=-1, n_iter=1)
-clf.fit(X_train_meta, Y_train_meta)
-pred = clf.predict(X_test_meta)
+            print("classification report:")
+            print(metrics.classification_report(le.inverse_transform(Y_test), le.inverse_transform(pred)))
+    else:
+        X_base, X_valid, Y_base, Y_valid = train_test_split(X, Y, test_size=0.1, random_state=42)
+        clf = LinearSVC()
+        clf.fit(X_base, Y_base)
+        pred = clf.predict(X_valid)
 
-score = metrics.f1_score(Y_valid, pred)
-print("f1-score:   %0.3f" % score)
+        score = metrics.f1_score(Y_valid, pred)
+        print("f1-score:   %0.3f" % score)
 
-print("classification report:")
-print(metrics.classification_report(le.inverse_transform(Y_valid), le.inverse_transform(pred)))
+        print("classification report:")
+        print(metrics.classification_report(le.inverse_transform(Y_valid), le.inverse_transform(pred)))
 
-joblib.dump(X_train_meta, 'blobs/X_train_meta_%s.pkl' % PREFIX)
-joblib.dump(Y_train_meta, 'blobs/Y_train_meta_%s.pkl' % PREFIX)
-joblib.dump(X_test_meta, 'blobs/X_test_meta_%s.pkl' % PREFIX)
-joblib.dump(Y_valid, 'blobs/Y_test_meta_%s.pkl' % PREFIX)
+if __name__ == '__main__':
+    main()
