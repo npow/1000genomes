540eed91e56c3a0bf1f3bb40ced29bc9eb4d843a
npow <nissan.pow@gmail.com>
2014-12-08 17:41:27 -0500

    use random projection

--- classify.py
+++ classify.py
@@ -15,6 +15,7 @@ from sklearn.linear_model import *
 from sklearn.neighbors import *
 from sklearn.pipeline import *
 from sklearn.preprocessing import *
+from sklearn.random_projection import *
 from sklearn.svm import *
 from sklearn.tree import *
 from sklearn import metrics
@@ -46,16 +47,22 @@ def classify(X, target):
 
     le = LabelEncoder()
     Y = le.fit_transform(Y)
-    joblib.dump(le, 'blobs/le%s.pkl' % target)
+    #joblib.dump(le, 'blobs/le%s.pkl' % target)
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
-    joblib.dump(Y_train, 'blobs/Y_train%s.pkl' % target)
-    joblib.dump(Y_test, 'blobs/Y_test%s.pkl' % target)
-    clf = LinearSVC()
-    clf.fit(X_train, Y_train)
+    #joblib.dump(Y_train, 'blobs/Y_train%s.pkl' % target)
+    #joblib.dump(Y_test, 'blobs/Y_test%s.pkl' % target)
 
-    joblib.dump(clf.decision_function(X_train), 'blobs/X_train_meta_%s%s.pkl' % (sys.argv[1], target))
-    joblib.dump(clf.decision_function(X_test), 'blobs/X_test_meta_%s%s.pkl' % (sys.argv[1], target))
+    clf = Pipeline([
+      ('s', SparseRandomProjection()),
+      ('c', LinearSVC())
+    ])
+    clf.fit(X_train, Y_train)
+    pred = clf.predict(X_test)
+    print metrics.f1_score(Y_test, pred)
+
+    joblib.dump(clf.decision_function(X_train), 'blobs/X_train_meta_rand_%s%s.pkl' % (sys.argv[1], target))
+    joblib.dump(clf.decision_function(X_test), 'blobs/X_test_meta_rand_%s%s.pkl' % (sys.argv[1], target))
 
 def main():
     chromosomes = [int(x) for x in sys.argv[1].split(',')]
